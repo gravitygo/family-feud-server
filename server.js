d592c061881c2303d254fc48c9f19a5b8439053b
gravitygo <chyle.andrei.lee@gmail.com>
2024-05-22 23:39:59 +0800

    serverless function

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+export default async function handler(req, res) {
+  if (!res.socket.server.wss) {
+    const wss = new WebSocket.Server({ noServer: true });
 
-let queue = [];
+    wss.on("connection", (ws) => {
+      console.log("Client connected");
 
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-  ws.on("message", (message) => {
-    const data = JSON.parse(message);
+      ws.on("message", (message) => {
+        const data = JSON.parse(message);
 
-    if (data.type === "pressButton") {
-      queue.push(data.color);
-      broadcastQueue();
-    } else if (data.type === "resetQueue") {
-      queue = [];
-      broadcastQueue();
+        if (data.type === "pressButton") {
+          queue.push(data.color);
+          broadcastQueue();
+        } else if (data.type === "resetQueue") {
+          queue = [];
+          broadcastQueue();
+        }
+        console.log(message);
+      });
+
+      ws.send(JSON.stringify({ type: "queue", queue }));
+    });
+
+    function broadcastQueue() {
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({ type: "queue", queue }));
+        }
+      });
     }
-    console.log(message);
-  });
-  ws.send(JSON.stringify({ type: "queue", queue }));
-});
 
-function broadcastQueue() {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ type: "queue", queue }));
-    }
-  });
+    res.socket.server.wss = wss;
+  }
+
+  res.end();
 }
-
-console.log("WebSocket server is running on ws://localhost:8080");
